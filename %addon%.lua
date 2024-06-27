327b8ffc53103f8217a914ee9c0d69a37792e851
Adriano Castro <adrianostanley@gmail.com>
2024-06-27 13:56:12 -0300

    Add the addon's main file

---
+++ %addon%.lua
@@ -0,0 +1,14 @@
+AddonTable = StormwindLibrary_vx_y_z.new({
+    colors = {
+      primary = '%hexadecimalColor%'
+    },
+    command = '%command%',
+    data = '%property%',
+    name = '%addonName%'
+  })
+
+local events = AddonTable.events
+
+events:listen(events.EVENT_NAME_PLAYER_LOGIN, function ()
+    -- %code here%
+end)
--- lib/stormwind-library.lua
+++ lib/stormwind-library.lua
@@ -1,1 +1,1 @@
-%code%
+-- %library code here%
